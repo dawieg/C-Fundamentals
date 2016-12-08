cd22a7fa753faa858aa4c3926d3e4dc64dba7754
Dawie Greyling <DawieG@directaxis.co.za>
2016-12-08 14:45:31 +0200

    First Commit

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your Name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you sleep last night <0-24>");
+            int sleep = int.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Hello, " + name);
+
+            if (sleep >= 8)
+            {
+                Console.WriteLine("You are well rested!!!");
+            }
+            else
+            {
+                Console.WriteLine("You need more rest!!!");
+            }
+        }
+    }
+}
