122086b50a98f69eca30ff66297a5627b720e04c
Dawie Greyling <DawieG@directaxis.co.za>
2016-12-09 14:04:07 +0200

    end end friday

--- Grades/Grades.Test/Types/ReferenceTypeTests.cs
+++ Grades/Grades.Test/Types/ReferenceTypeTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,6 +11,17 @@ namespace Grades.Test.Types
     [TestClass]
     public class ReferenceTypeTests
     {
+
+        [TestMethod]
+        public void StringComparisons()
+        {
+            string name1 = "Scott";
+            string name2 = "scott";
+
+            bool result = string.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
         [TestMethod]
         public void IntVariablesHoldValue()
         {
