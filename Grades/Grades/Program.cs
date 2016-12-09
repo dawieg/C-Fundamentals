d725aa6810d4fa175a55055ae867c0bc4d6471fe
Dawie Greyling <DawieG@directaxis.co.za>
2016-12-09 08:58:30 +0200

    Complete

---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            GradeBook book =new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine("Avarage GradeBook" + " :" +stats.AverageGrade);
+            Console.WriteLine("Highest GradeBook" + " :" + stats.HighestGrade);
+            Console.WriteLine("Lowest GradeBook" + " :" + stats.LowestGrade);
+            
+        }
+    }
+}
