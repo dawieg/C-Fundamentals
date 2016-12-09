077e73416e42864fb1cd8f576528cf82d044f294
Dawie Greyling <DawieG@directaxis.co.za>
2016-12-09 07:50:47 +0200

    commit 1

---
+++ GradeBook/GradeBook/Gradebook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Gradebook
+    {
+        
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        private List<float> grades;
+    }
+}
