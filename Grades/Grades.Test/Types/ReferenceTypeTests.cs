02a478dbbe76be800ac80779daba6e677e768100
Dawie Greyling <DawieG@directaxis.co.za>
2016-12-09 12:57:09 +0200

    End of Friday

---
+++ Grades/Grades.Test/Types/ReferenceTypeTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grades.Test.Types
+{
+    [TestClass]
+    public class ReferenceTypeTests
+    {
+        [TestMethod]
+        public void IntVariablesHoldValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1,x2);
+        }
+        
+        [TestMethod]
+        public void GradebookVariableHoldReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "Scott's Grate book";
+
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+    }
+}
--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -10,15 +10,16 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-        
-            GradeBook book =new GradeBook();
+
+           
+            GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
 
-            Console.WriteLine("Avarage GradeBook" + " :" +stats.AverageGrade);
+            Console.WriteLine("Avarage GradeBook" + " :" + stats.AverageGrade);
             Console.WriteLine("Highest GradeBook" + " :" + stats.HighestGrade);
             Console.WriteLine("Lowest GradeBook" + " :" + stats.LowestGrade);
             
