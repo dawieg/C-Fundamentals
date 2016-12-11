ecbcaa70bf932702f4f420a28750221a907b9283
Dawie Greyling <DawieG@directaxis.co.za>
2016-12-11 08:20:32 +0200

    Types done

--- Grades/Grades.Test/Types/ReferenceTypeTests.cs
+++ Grades/Grades.Test/Types/ReferenceTypeTests.cs
@@ -9,8 +9,75 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Grades.Test.Types
 {
     [TestClass]
-    public class ReferenceTypeTests
+    public class TypeTests
     {
+
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            addGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void addGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "scott";
+            name = name.ToUpper();
+
+            Assert.AreEqual("SCOTT", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2015, 01, 03);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(4, date.Day);
+        }
+
+        [TestMethod]
+        public void ValueTypesPassByValue()
+        {
+            int x = 46;
+
+            IncrementNumber(ref x);
+            Assert.AreEqual(47, x);
+        
+        }
+
+        private void IncrementNumber(ref int number)
+        {
+            number += 1;
+        }
+
+
+        [TestMethod]
+        public void ReferenceTypesPassByValues()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+
+            GiveBookAName(ref book2);
+            Assert.AreEqual("A GradeBook", book2.Name);
+        }
+
+        private void GiveBookAName(ref GradeBook book)
+        {
+            book = new GradeBook();
+            book.Name = "A GradeBook";
+        }
 
         [TestMethod]
         public void StringComparisons()
