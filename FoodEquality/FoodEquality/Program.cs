32f6fb9f70dab8d43e521d1509fd51ef6cdea941
Dawie Greyling <DawieG@directaxis.co.za>
2017-01-06 09:16:23 +0200

    Food first commit

---
+++ FoodEquality/FoodEquality/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Food banana = new Food ("banana");
+            Food banana2 = new Food("banana");
+            Food chocolate = new Food("chocolate");
+
+            Console.WriteLine("object banana = " + banana);
+            Console.WriteLine("object banana2 = " + banana2);
+            Console.WriteLine("object chocolate = " + chocolate);
+            
+            Console.WriteLine(banana.Equals(chocolate));
+            Console.WriteLine(banana.Equals(banana2));
+            Console.WriteLine(banana.Equals(null));
+            
+        }
+
+    }
+}
